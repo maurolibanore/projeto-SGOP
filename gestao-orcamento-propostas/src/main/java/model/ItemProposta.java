59c17edc43c7709f3de0b931bfff20276a347197
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-11-24 23:24:48 -0300

    feat: itemproposta

--- gestao-orcamento-propostas/src/main/java/model/ItemProposta.java
+++ gestao-orcamento-propostas/src/main/java/model/ItemProposta.java
@@ -1,5 +1,87 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "item_proposta")
 public class ItemProposta {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @ManyToOne
+    @JoinColumn(name = "proposta_id", nullable = false)
+    private Proposta proposta;
+
+    @ManyToOne
+    @JoinColumn(name = "servico_id", nullable = false)
+    private Servico servico;
+
+    private Double precoUnitarioMomento; 
+    private Integer quantidade;
+    private Double subtotal; 
+
+    public void calcularSubtotal() {
+        if (this.precoUnitarioMomento != null && this.quantidade != null) {
+            this.subtotal = this.precoUnitarioMomento * this.quantidade;
+        } else {
+            this.subtotal = 0.0;
+        }
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Proposta getProposta() {
+        return proposta;
+    }
+
+    public void setProposta(Proposta proposta) {
+        this.proposta = proposta;
+    }
+
+    public Servico getServico() {
+        return servico;
+    }
+
+    public void setServico(Servico servico) {
+        this.servico = servico;
+    }
+
+    public Double getPrecoUnitarioMomento() {
+        return precoUnitarioMomento;
+    }
+
+    public void setPrecoUnitarioMomento(Double precoUnitarioMomento) {
+        this.precoUnitarioMomento = precoUnitarioMomento;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
