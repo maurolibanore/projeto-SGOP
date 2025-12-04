e623a162d3191dcaa4607f17822fc5dd262fd0b2
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-12-03 23:55:35 -0300

    feat:PropostaDAO implementacao

--- gestao-orcamento-propostas/src/main/java/dao/PropostaDAO.java
+++ gestao-orcamento-propostas/src/main/java/dao/PropostaDAO.java
@@ -1,5 +1,21 @@
 package dao;
 
-public class PropostaDAO {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import model.Proposta;
+import util.JPAUTIL;
+
+public class PropostaDAO extends DaoImplementacao<Proposta> {
+
+	public PropostaDAO(Class<Proposta> classe) {
+		super(classe);
+	}
+	
+	public List<Proposta> buscarPorCliente(Long id){
+		EntityManager em = JPAUTIL.getEntityManager();
+		return em.createQuery("SELECT p FROM Proposta p WHERE p.cliente.id = :id",Proposta.class).setParameter("id",id).getResultList();
+	}
 
 }
