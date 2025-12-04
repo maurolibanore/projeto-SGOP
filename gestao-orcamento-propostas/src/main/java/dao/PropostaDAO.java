24537964f63042e29f772aff3bb18f583834dfaf
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-12-03 23:57:19 -0300

    feat: finalizacao PropostaDAO

--- gestao-orcamento-propostas/src/main/java/dao/PropostaDAO.java
+++ gestao-orcamento-propostas/src/main/java/dao/PropostaDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import model.Proposta;
+import model.StatusProposta;
 import util.JPAUTIL;
 
 public class PropostaDAO extends DaoImplementacao<Proposta> {
@@ -18,4 +19,9 @@ public class PropostaDAO extends DaoImplementacao<Proposta> {
 		return em.createQuery("SELECT p FROM Proposta p WHERE p.cliente.id = :id",Proposta.class).setParameter("id",id).getResultList();
 	}
 
+	public List<Proposta> buscarPorStatus(StatusProposta status){
+		EntityManager em = JPAUTIL.getEntityManager();
+		return em.createQuery("SELECT p FROM Proposta p WHERE p.status = :status", Proposta.class).setParameter("status", status).getResultList();
+	}
+	
 }
