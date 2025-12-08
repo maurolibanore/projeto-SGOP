50b1e4382790d9917badf3d4e38e08f6e01307a5
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-12-08 00:43:48 -0300

    refactor: construtor

--- gestao-orcamento-propostas/src/main/java/service/RelatorioService.java
+++ gestao-orcamento-propostas/src/main/java/service/RelatorioService.java
@@ -1,5 +1,43 @@
 package service;
 
-public class RelatorioService {
+import java.time.LocalDate;
+import java.util.List;
 
+import dao.PropostaDAO;
+
+public class RelatorioService {
+	private PropostaDAO dao;
+
+    public RelatorioService() {
+        this.dao = new PropostaDAO();
+    }
+
+    public void gerarRelatorioVendasPorPeriodo(LocalDate inicio, LocalDate fim) {
+        List<Proposta> propostas = dao.buscarPorPeriodo(inicio, fim);
+        
+        System.out.println("--- Relatório de Propostas (" + inicio + " a " + fim + ") ---");
+        
+        double totalFaturado = 0.0;
+        
+        for (Proposta p : propostas) {
+            System.out.println("ID: " + p.getId() + " | Cliente: " + p.getCliente().getNome() + 
+                               " | Valor: R$ " + p.getValorTotal() + " | Status: " + p.getStatus());
+            
+            if (p.getStatus() == StatusProposta.ACEITA) {
+                totalFaturado += p.getValorTotal();
+            }
+        }
+        
+        System.out.println("------------------------------------------------");
+        System.out.println("Total Faturado (ACEITAS): R$ " + totalFaturado);
+    }
+    
+    public void gerarRelatorioPorCliente(Long idCliente) {
+        List<Proposta> propostas = dao.buscarPorCliente(idCliente);
+        
+        System.out.println("--- Histórico do Cliente ID: " + idCliente + " ---");
+        for (Proposta p : propostas) {
+            System.out.println("Data: " + p.getDataCriacao() + " | Valor: " + p.getValorTotal() + " - " + p.getStatus());
+        }
+    }
 }
