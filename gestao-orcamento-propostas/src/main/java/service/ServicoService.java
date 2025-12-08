05ab8346a35c081741224a63a67cfe9b95d45115
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-12-07 23:20:40 -0300

    feat: implementacao ServicoService somente 1 regra

--- gestao-orcamento-propostas/src/main/java/service/ServicoService.java
+++ gestao-orcamento-propostas/src/main/java/service/ServicoService.java
@@ -1,5 +1,42 @@
 package service;
 
+import java.time.LocalDate;
+
+import dao.ServicoDAO;
+import model.Preco;
+import model.Servico;
+
 public class ServicoService {
+	
+	private ServicoDAO dao;
+	
+	public ServicoService() {
+		this.dao = new ServicoDAO();
+	}
+	
+	public void cadastrarServico(String descricao, Double valorInicial) {
+		//valida descricao unica
+		if(dao.buscarPorDescricao(descricao)!= null) {
+			throw new IllegalArgumentException("Já existe um serviço com essa descricao");	
+		}
+		
+		Servico servico = new Servico(descricao);
+		
+		// RN preco ativo primeiro
+		Preco preco = new Preco(valorInicial, LocalDate.now(), true, servico);
+		servico.getHistoricoPrecos().add(preco);
+		
+		dao.cadastrar(servico);
+	}
+	
+	//RN REAJUSTE EM MASSA
+
+	
 
 }
+
+
+
+
+
+
