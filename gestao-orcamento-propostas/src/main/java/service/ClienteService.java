852935561e2e89ba311607c3f86d38190a0d65ed
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-12-07 22:22:51 -0300

    feat: clienteService

--- gestao-orcamento-propostas/src/main/java/service/ClienteService.java
+++ gestao-orcamento-propostas/src/main/java/service/ClienteService.java
@@ -1,5 +1,42 @@
 package service;
 
+import dao.ClientePessoaFisicaDAO;
+import dao.ClientePessoaJuridicaDAO;
+import model.ClientePessoaFisica;
+import model.ClientePessoaJuridica;
+
 public class ClienteService {
+	
+	private ClientePessoaFisicaDAO pfDAO;
+	private ClientePessoaJuridicaDAO pjDAO;
+	
+	public ClienteService () {
+		this.pfDAO = new ClientePessoaFisicaDAO();
+		this.pjDAO = new ClientePessoaJuridicaDAO();
+	
+	}
+	
+	// RN : nao permite cpf duplicado
+	public void cadastrarPessoaFisica(ClientePessoaFisica cliente) {
+		ClientePessoaFisica existente = pfDAO.buscarPorCpf(cliente.getCpf());
+		
+		if(existente != null) {
+			throw new IllegalArgumentException("CPF já cadastrado");
+		}
+		// caso nao exista cadastra
+		pfDAO.cadastrar(cliente);
+	}
+	
+	
+	// cnpj duplicado
+	public void cadastrarPessoaJuridica(ClientePessoaJuridica cliente) {
+		ClientePessoaJuridica existente = pjDAO.buscarPorCnpj(cliente.getCnpj());
+		
+		if(existente != null) {
+			throw new IllegalArgumentException("CNPJ já cadastrado");
+		}
+		pjDAO.cadastrar(cliente);
+	}
+	
 
 }
