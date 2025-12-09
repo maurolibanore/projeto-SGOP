46c9a6d58b1b5fd273cfcfde2c60e1fa04dbff3b
Mauro Libanore <162159200+maurolibanore@users.noreply.github.com>
2025-12-09 10:52:57 -0300

    fix: arrumando erro quando nao acha nada

--- gestao-orcamento-propostas/src/main/java/dao/ServicoDAO.java
+++ gestao-orcamento-propostas/src/main/java/dao/ServicoDAO.java
@@ -1,22 +1,29 @@
 package dao;
 
 import javax.persistence.EntityManager;
-
+import javax.persistence.NoResultException; 
 import model.Servico;
 import util.JPAUTIL;
 
 public class ServicoDAO extends DaoImplementacao<Servico> {
 
-	public ServicoDAO() {
-		super(Servico.class);
-	}
-	
-	public Servico buscarPorDescricao(String descricao) {
-		EntityManager em = JPAUTIL.getEntityManager();
-		
-		return em.createQuery("SELECT s FROM Servico s WHERE s.descricao = :n", Servico.class).setParameter("n", descricao).getSingleResult();
-	}
-	
-	
-
+    public ServicoDAO() {
+        super(Servico.class);
+    }
+    
+    public Servico buscarPorDescricao(String descricao) {
+        EntityManager em = JPAUTIL.getEntityManager(); 
+        
+        try {
+            return em.createQuery("SELECT s FROM Servico s WHERE s.descricao = :n", Servico.class)
+                     .setParameter("n", descricao)
+                     .getSingleResult();
+                     
+        } catch (NoResultException e) {
+            return null;
+            
+        } finally {
+            em.close(); 
+        }
+    }
 }
